3a6ad8b020654f4b45bdf6e9472e5d5e5f6cae3f
Ratnadeep091295 <126142707+Ratnadeep091295@users.noreply.github.com>
2023-02-26 01:39:05 +0530

    Add files via upload

---
+++ js/dashboard.js
@@ -0,0 +1,131 @@
+
+        $(document).ready(function () {
+
+            /*----------SIDEBAR------------*/
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active');
+            });
+
+/*----------START HEADER RIGHT------------*/
+
+        const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+        });
+
+/*----------HEADER SEARCH------------*/
+
+           $(document).ready(function(){
+            var submitIcon = $('.searchbox-icon');
+            var inputBox = $('.searchbox-input');
+            var searchBox = $('.searchbox');
+            var isOpen = false;
+            submitIcon.click(function(){
+                if(isOpen == false){
+                    searchBox.addClass('searchbox-open');
+                    inputBox.focus();
+                    isOpen = true;
+                } else {
+                    searchBox.removeClass('searchbox-open');
+                    inputBox.focusout();
+                    isOpen = false;
+                }
+            });  
+             submitIcon.mouseup(function(){
+                    return false;
+                });
+            searchBox.mouseup(function(){
+                    return false;
+                });
+            $(document).mouseup(function(){
+                    if(isOpen == true){
+                        $('.searchbox-icon').css('display','block');
+                        submitIcon.click();
+                    }
+                });
+        });
+            function buttonUp(){
+                var inputVal = $('.searchbox-input').val();
+                inputVal = $.trim(inputVal).length;
+                if( inputVal !== 0){
+                    $('.searchbox-icon').css('display','none');
+                } else {
+                    $('.searchbox-input').val('');
+                    $('.searchbox-icon').css('display','block');
+                }
+            }
+
+/**
+     * EFECTO PARA FLECHAS EN ACORDEON
+     */
+    $('.panel-collapse.in').collapse('hide', function() {
+  $('#collapseOne').collapse('show');
+});
+$('.panel-collapse').on('show.bs.collapse', function (e) {
+  $(e.target).closest('.panel').siblings().find('.panel-collapse').collapse('hide');
+});
+  
+
+  
+  
+$('#accordion').on('shown.bs.collapse', function () {
+  
+  var panel = $(this).find('.show');
+  
+  $('html, body').animate({
+        scrollTop: panel.offset().top
+  }, 500);
+  
+});
+
+
+
+
+
+
+
+
+
+/*----------MULTIPLE IMAGE UPLOAD------------*/
+
+
+
+
+
+
+
+
+// Upload image preview js 
+  
+    $(document).on('click', '.browse', function () {
+        var file = $(this).parent().parent().parent().find('.file');
+        file.trigger('click');
+    });
+    $(document).on('change', '.file', function () {
+        $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+    });
+
+ 
